e659774f40fedee0bd10ea467d8034a1ca3af312
srt1017 <omarfarouq107@gmail.com>
2023-05-29 17:14:10 -0500

    added functionality to sort games

--- game-hub/src/hooks/useGames.tsx
+++ game-hub/src/hooks/useGames.tsx
@@ -19,7 +19,13 @@ interface Game {
 const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
-    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id } },
+    {
+      params: {
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder
+      },
+    },
     [gameQuery]
   );
 
