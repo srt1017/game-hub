cafe031f28abdbdc3ed82e90ea9f5839d051ba55
srt1017 <omarfarouq107@gmail.com>
2023-06-14 13:02:36 -0500

    Set up routing

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import App from './App.tsx'
 import theme from './theme.ts'
 import './index.css'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes.tsx'
 
 const queryClient = new QueryClient()
 
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initalColorMode}></ColorModeScript>
       <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
     <ReactQueryDevtools></ReactQueryDevtools>
     </QueryClientProvider>
     </ChakraProvider>
---
+++ game-hub/src/pages/HomePage.tsx
@@ -0,0 +1,41 @@
+import { Grid, Show, GridItem, Flex, Box } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"main"`,
+        lg: `"aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX="5px">
+          <GenreList></GenreList>
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={4}>
+          <GameHeading></GameHeading>
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector></PlatformSelector>
+            </Box>
+            <SortSelector></SortSelector>
+          </Flex>
+        </Box>
+        <GameGrid></GameGrid>
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
