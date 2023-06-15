c6ee19648f857399a3e87e02b4dbee4ccb04dfa6
srt1017 <omarfarouq107@gmail.com>
2023-06-15 18:48:17 -0500

    fix

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import theme from './theme.ts'
-import './index.css'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./theme.ts";
+import "./index.css";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initalColorMode}></ColorModeScript>
+      <ColorModeScript
+        initialColorMode={theme.config.initalColorMode}
+      ></ColorModeScript>
       <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-    <ReactQueryDevtools></ReactQueryDevtools>
-    </QueryClientProvider>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ChakraProvider>
-
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
